080f7f69a584c613ea14529ddf71a67ef80d9088
sajidjabbar <93922377+sajidjabbar@users.noreply.github.com>
2022-07-21 01:51:58 +0500

    chnages

--- inc/footerlinks.php
+++ inc/footerlinks.php
@@ -1,15 +1,15 @@
 <!-- Jquery JS-->
 <script src="https://code.jquery.com/jquery-3.6.0.min.js" type="text/javascript"></script>
 <!-- Vendor JS Files -->
-<script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+<!--<script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>-->
 <script src=""></script>
-<script src="js/aos.js"></script>
-<script src="js/bootstrap.bundle.min.js"></script>
-<script src="js/glightbox.min.js"></script>
-<script src="js/isotope.pkgd.min.js"></script>
-<script src="js/swiper-bundle.min.js"></script>
-<script src="js/noframework.waypoints.js"></script>
-<script src="js/validate.js"></script>
+<!--<script src="js/aos.js"></script>-->
+<!--<script src="js/bootstrap.bundle.min.js"></script>-->
+<!--<script src="js/glightbox.min.js"></script>-->
+<!--<script src="js/isotope.pkgd.min.js"></script>-->
+<!--<script src="js/swiper-bundle.min.js"></script>-->
+<!--<script src="js/noframework.waypoints.js"></script>-->
+<!--<script src="js/validate.js"></script>-->
 
 <!-- Template Main JS File -->
 <script src="js/main.js"></script>
--- inc/header.php
+++ inc/header.php
@@ -2,54 +2,60 @@
 <section id="topbar" class="d-flex align-items-center">
     <div class="container d-flex justify-content-center justify-content-md-between">
         <div class="contact-info d-flex align-items-center">
-            <i class="bi bi-envelope d-flex align-items-center"><a href="mailto:contact@example.com">contact@example.com</a></i>
-            <i class="bi bi-phone d-flex align-items-center ms-4"><span>+1 5589 55488 55</span></i>
+            <i class="bi bi-envelope d-flex align-items-center"><span>Email Us: <br>someone@gmail.com</span></i>
+            <i class="bi bi-clock d-flex align-items-center ms-4 ml-4"><span>Opening Time: <br>SUn - Fri: 8.30 - 9:30</span></i>
         </div>
-        <div class="social-links d-none d-md-flex align-items-center">
-            <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
-            <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
-            <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
-            <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></i></a>
+        <div class=" contact-info d-md-flex align-items-center">
+            <i class="bi bi-geo-alt d-flex align-items-center"><span>Contact Us: <br>Square, Square Street,UK</span></i>
+            <i class="bi bi-person d-flex align-items-center ms-4 ml-4"><span>User Account: <br>Sign In/ Sign Up</span></i>
         </div>
     </div>
 </section>
 
 <!-- ======= Header ======= -->
 <header id="header" class="d-flex align-items-center">
-    <div class="container d-flex align-items-center justify-content-between">
+    <div class="container d-flex align-items-left justify-content-left">
 
-        <h1 class="logo"><a href="index.html">BizLand<span>.</span></a></h1>
+        <h1 class="logo"><a href="index.html"><img class="img-fluid" src="img/logo.png"></a></h1>
         <!-- Uncomment below if you prefer to use an image logo -->
         <!-- <a href="index.html" class="logo"><img src="assets/img/logo.png" alt=""></a>-->
 
         <nav id="navbar" class="navbar">
             <ul>
-                <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
-                <li><a class="nav-link scrollto" href="#about">About</a></li>
-                <li><a class="nav-link scrollto" href="#services">Services</a></li>
-                <li><a class="nav-link scrollto " href="#portfolio">Portfolio</a></li>
-                <li><a class="nav-link scrollto" href="#team">Team</a></li>
-                <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
-                    <ul>
-                        <li><a href="#">Drop Down 1</a></li>
-                        <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
-                            <ul>
-                                <li><a href="#">Deep Drop Down 1</a></li>
-                                <li><a href="#">Deep Drop Down 2</a></li>
-                                <li><a href="#">Deep Drop Down 3</a></li>
-                                <li><a href="#">Deep Drop Down 4</a></li>
-                                <li><a href="#">Deep Drop Down 5</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="#">Drop Down 2</a></li>
-                        <li><a href="#">Drop Down 3</a></li>
-                        <li><a href="#">Drop Down 4</a></li>
-                    </ul>
-                </li>
-                <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
+                <li><a class="nav-link scrollto active" href="#hero">HOME</a></li>
+                <li><a class="nav-link scrollto" href="#about">ABOUT</a></li>
+                <li><a class="nav-link scrollto" href="#services">TESTIMONIALS</a></li>
+                <li><a class="nav-link scrollto " href="#portfolio">PRIVACY POLICY</a></li>
+<!--                <li><a class="nav-link scrollto" href="#team">Team</a></li>-->
+<!--                <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>-->
+<!--                    <ul>-->
+<!--                        <li><a href="#">Drop Down 1</a></li>-->
+<!--                        <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>-->
+<!--                            <ul>-->
+<!--                                <li><a href="#">Deep Drop Down 1</a></li>-->
+<!--                                <li><a href="#">Deep Drop Down 2</a></li>-->
+<!--                                <li><a href="#">Deep Drop Down 3</a></li>-->
+<!--                                <li><a href="#">Deep Drop Down 4</a></li>-->
+<!--                                <li><a href="#">Deep Drop Down 5</a></li>-->
+<!--                            </ul>-->
+<!--                        </li>-->
+<!--                        <li><a href="#">Drop Down 2</a></li>-->
+<!--                        <li><a href="#">Drop Down 3</a></li>-->
+<!--                        <li><a href="#">Drop Down 4</a></li>-->
+<!--                    </ul>-->
+<!--                </li>-->
+<!--                <li><a class="nav-link scrollto" href="#contact">Contact</a></li>-->
             </ul>
             <i class="bi bi-list mobile-nav-toggle"></i>
         </nav><!-- .navbar -->
-
+        <div class="d-flex align-items-center justify-content-center search-section">
+            <i class="bi bi-search mr-3"></i>
+            <div class="mr-3 call-text">
+                <p>Call us for any question</p>
+                <span>(778) 7724087</span>
+            </div>
+            <a href="javascript:void(0);" class="btn-get-started scrollto">Contact Us</a>
+        </div>
     </div>
+
 </header><!-- End Header -->
