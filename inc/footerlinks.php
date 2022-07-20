7cb1adb627cbceb174ebfbac33c3f6d8f9b4f587
sajidjabbar <93922377+sajidjabbar@users.noreply.github.com>
2022-07-20 22:35:07 +0500

    initial

---
+++ inc/footerlinks.php
@@ -0,0 +1,23 @@
+<!-- Jquery JS-->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" type="text/javascript"></script>
+<!-- Vendor JS Files -->
+<script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+<script src=""></script>
+<script src="js/aos.js"></script>
+<script src="js/bootstrap.bundle.min.js"></script>
+<script src="js/glightbox.min.js"></script>
+<script src="js/isotope.pkgd.min.js"></script>
+<script src="js/swiper-bundle.min.js"></script>
+<script src="js/noframework.waypoints.js"></script>
+<script src="js/validate.js"></script>
+
+<!-- Template Main JS File -->
+<script src="js/main.js"></script>
+<!-- Jquery JS -->
+<script>
+    $(document).ready(function () {
+        $(window).on('load', function(){
+            $("#preloader").fadeOut(1000);
+        });
+    })
+</script>
---
+++ inc/headlinks.php
@@ -0,0 +1,26 @@
+<meta charset="utf-8">
+<meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+<title>Arian</title>
+<meta content="" name="description">
+<meta content="" name="keywords">
+
+<!-- Favicons -->
+<link href="assets/img/favicon.png" rel="icon">
+<link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+<!-- Google Fonts -->
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" rel="stylesheet">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+
+<!-- Vendor CSS Files -->
+<link rel="stylesheet" href="css/aos.css">
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link href="css/bootstrap-icons.css" rel="stylesheet">
+<link href="css/boxicons.min.css" rel="stylesheet">
+<link href="css/glightbox.min.css" rel="stylesheet">
+<link href="css/swiper-bundle.min.css" rel="stylesheet">
+
+<!-- Template Main CSS File -->
+<link href="css/style.css" rel="stylesheet">
