b549f684ec58cf4dde3b1c6ba641a0546817d6f2
sufyaninovio <105053286+sufyaninovio@users.noreply.github.com>
2022-07-27 09:03:44 -0700

    2.5 pages

---
+++ about.php
@@ -0,0 +1,181 @@
+<?php include 'inc/app.php'; ?>
+<link rel="stylesheet" href="css/about.css">
+<!-- Custom Banner -->
+<section class="custom_banner">
+    <div class="container">
+        <div class="custom_banner_text">
+            <h2>About Us</h2>
+            <p>
+                <a href="index.php">Home</a>
+                <img src="img/inner.png" alt="">
+                <a href="about.php">About Us</a>
+            </p>
+        </div>
+    </div>
+</section>
+<!-- End Here -->
+<!-- TEXT SECtion -->
+<section class="about_text">
+    <div class="container">
+        <div class="top_heading text-center">
+            <h6 class="blue">About Arian Home Care</h6>
+            <h4 class="h4">Who We Are</h4>
+        </div>
+        <div class="text_start mt-5">
+            <p class="mb-4"> We are a small local family business providing non-medical care and assistance to seniors, children, and
+                special needs individuals. Our team consists of infinite compassion, respect, love, and support. The
+                quality of care that we provide for your loved ones is just the same with our own family by going beyond
+                given responsibilities for a consistent smile and satisfaction of you and your family.
+            </p>
+            <p class="mb-4">
+                We have a team of experts on care aid, nursing, and other related background experience dedicated to our
+                senior and special needs care clients, and a separate team with qualifications explicit to children of
+                12 years old and under, as well as mother support with infant care.
+            </p>
+            <p class="mb-4">
+                At Arian Homecare, relationship and building a unique bond is key to increase comfort in our work
+                together. Our clients will be receiving a dedicated care provider as their support worker which they can
+                communicate with directly on their needs.
+            </p>
+            <p class="mb-4">
+                We aspire to make things happen for our clients. We are not a large corporate with a strict system,
+                therefore we can be flexible to work with different budget and service needs to accomplish what works
+                best for our clients. Contact us at (778) 7724087 for a free consultation on how we fulfill the support
+                you need.
+            </p>
+        </div>
+    </div>
+</section>
+<!-- About Text END Here -->
+<!-- OUR TEAM START HERE -->
+<section class="out_team">
+    <div class="container">
+        <div class="col-md-7 mx-auto">
+            <div class="our_team_text text-center">
+                <h6 class="blue">Our Team</h6>
+                <h4 class="h4">Meet the owners of Arian Homecare</h4>
+                <p>Arian Homecare is a Personal Assistance Care Agency founded in 2017 by two females, mother and daughter, Kamelia and Kimia.</p>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="single_partner">
+                        <figure>
+                            <img src="img/partner1.png" class="img-fluid" alt="">
+                        </figure>
+                        <div class="partner_detail mx-auto">
+                            <h6>Kamelia Aria</h6>
+                            <p class="mb-0">Client Care Manager, Owner</p>
+                            <span>(778) 7724087</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="single_partner">
+                        <figure>
+                            <img src="img/partner2.png" class="img-fluid" alt="">
+                        </figure>
+                        <div class="partner_detail mx-auto">
+                            <h6>Kimia Jarrahian</h6>
+                            <p class="mb-0">Client Care Manager, Owner</p>
+                            <span>(778) 7724087</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- OUR TEAM END HERE -->
+<!-- contact Section Start Here -->
+<section class="contact_section_start_here">
+    <div class="container-fluid">
+        <div class="custom_width mx-auto">
+            <div class="row">
+                <div class="col-md-5">
+                    <div class="contact_text">
+                        <div class="top_text">
+                            <h6 class="blue">Limited Time Offer</h6>
+                            <h4 class="mt-3 pb-4">Contact Us Today And Get 100% <br> Free Consultaion</h4>
+                        </div>
+                        <div class="contact_us_on">
+                            <div class="single_contact">
+                                <figure>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="32.129" height="32.177" viewBox="0 0 32.129 32.177">
+                                        <path id="Path_148" data-name="Path 148" d="M-265.345-366.637a16.183,16.183,0,0,1-4.214-.987,29.582,29.582,0,0,1-8.458-5.007,36.445,36.445,0,0,1-8.856-10.436,21.21,21.21,0,0,1-2.49-6.748,2.664,2.664,0,0,1,.328-1.983,26.386,26.386,0,0,1,5.308-6.3c.136-.115.287-.212.432-.316a1.8,1.8,0,0,1,2.465.2,1.538,1.538,0,0,1,.3.315,41.176,41.176,0,0,1,3.963,6.511,9.071,9.071,0,0,1,.4,1.1,1.582,1.582,0,0,1-.346,1.626c-.519.551-1.088,1.056-1.657,1.556-.752.66-.77.653-.374,1.577a14.664,14.664,0,0,0,8.414,8.16.608.608,0,0,0,.843-.2c.451-.581.952-1.125,1.447-1.671a1.984,1.984,0,0,1,2.481-.56,35.64,35.64,0,0,1,3.216,1.632c1.312.782,2.565,1.663,3.833,2.516a2.377,2.377,0,0,1,.972,1.438,1.217,1.217,0,0,1-.115.969,7.119,7.119,0,0,1-.709,1.052,25.386,25.386,0,0,1-6.424,5.259A4.414,4.414,0,0,1-265.345-366.637Z" transform="translate(289.422 398.814)" fill="#3cbffc" />
+                                    </svg>
+                                </figure>
+                                <div class="contact_here">
+                                    <h5 class="mb-0">Call Us :</h5>
+                                    <a href="" class="robot">778-927-1348</a>
+                                </div>
+                            </div>
+                            <div class="single_contact">
+                                <figure>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="30.129" height="21.231" viewBox="0 0 30.129 21.231">
+                                        <g id="Group_102" data-name="Group 102" transform="translate(732.233 327.033)">
+                                            <path id="Path_5042" data-name="Path 5042" d="M-458.509-289.379l-.245.75-9.1-9.038,9.116-9.043.229.7Z" transform="translate(-243.595 -18.725)" fill="#3cbffc" />
+                                            <path id="Path_5043" data-name="Path 5043" d="M-712.838-326.817c.356-.077.657-.147.959-.205a1.567,1.567,0,0,1,.294-.008q12.282,0,24.563.007a9.018,9.018,0,0,1,1.2.159l.013.107c-.071.074-.141.15-.213.223l-12.42,12.482a1.056,1.056,0,0,1-1.706.009l-12.44-12.5C-712.654-326.615-712.717-326.688-712.838-326.817Z" transform="translate(-17.87)" fill="#3cbffc" />
+                                            <path id="Path_5044" data-name="Path 5044" d="M-694.487-175.189l9.138,9.066c-.358.079-.656.151-.957.207a1.931,1.931,0,0,1-.352.009h-24.47a2.638,2.638,0,0,1-1.271-.26l9.1-9.033c.028.026.106.1.18.169q1.08,1.085,2.159,2.171a2.787,2.787,0,0,0,4.131,0C-696.035-173.649-695.242-174.437-694.487-175.189Z" transform="translate(-18.275 -139.899)" fill="#3cbffc" />
+                                            <path id="Path_5045" data-name="Path 5045" d="M-732.026-289.232a5.739,5.739,0,0,1-.2-1.06q-.019-8,0-16a5.969,5.969,0,0,1,.2-1.073l9.134,9.072Z" transform="translate(0 -18.121)" fill="#3cbffc" />
+                                        </g>
+                                    </svg>
+                                </figure>
+                                <div class="contact_here">
+                                    <h5 class="mb-0">Email :</h5>
+                                    <a href="" class="robot">info@arianhomecare.com</a>
+                                </div>
+                            </div>
+                            <div class="single_contact">
+                                <figure>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="22.566" height="30.729" viewBox="0 0 22.566 30.729">
+                                        <g id="Group_103" data-name="Group 103" transform="translate(-233.838 390.521)">
+                                            <path id="Path_5046" data-name="Path 5046" d="M245.143-390.521a11.328,11.328,0,0,1,11.136,9.479,9.835,9.835,0,0,1-.6,5.2,41.512,41.512,0,0,1-3.938,7.512,97.944,97.944,0,0,1-5.728,8.046,1.028,1.028,0,0,1-1.734.03,85.238,85.238,0,0,1-7.955-11.813,23.159,23.159,0,0,1-2.216-5.322,9.441,9.441,0,0,1,.667-6.3,11.089,11.089,0,0,1,8.581-6.68,6.275,6.275,0,0,1,.635-.077C244.376-390.474,244.759-390.5,245.143-390.521Zm-4.112,10.238a4.1,4.1,0,0,0,4.083,4.1,4.106,4.106,0,0,0,4.121-4.066,4.1,4.1,0,0,0-4.068-4.12A4.1,4.1,0,0,0,241.031-380.283Z" fill="#3cbffc" />
+                                        </g>
+                                    </svg>
+                                </figure>
+                                <div class="contact_here">
+                                    <h5 class="mb-0">Address :</h5>
+                                    <span class="robot roboto_21">#201-252 Esplanade W, North Vancouver,<br>BC, V7M 3G7</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-7">
+                    <div class="contact_form">
+                        <div class="form_top_text">
+                            <h6 class="blue">Get In Touch</h6>
+                            <h4>Contact Us</h4>
+                        </div>
+                        <form action="" method="POST">
+                            <div class="form_row">
+                                <div class="single_input">
+                                    <input type="text" name="" id="" placeholder="Enter Name">
+                                </div>
+                                <div class="single_input">
+                                    <input type="text" name="" id="" placeholder="Enter Email">
+                                </div>
+                            </div>
+                            <div class="form_row">
+                                <div class="single_input">
+                                    <input type="text" name="" id="" placeholder="Enter Phone Number">
+                                </div>
+                                <div class="single_input">
+                                    <input type="text" name="" id="" placeholder="Enter Subject">
+                                </div>
+                            </div>
+                            <div class="form_row">
+                                <textarea name="" id="" placeholder="Enter Your Message"></textarea>
+                            </div>
+                            <div class="submit_btn">
+                                <button type="submit" class="btn">SUBMIT</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- contact Section End Here -->
+<?php include 'inc/footer.php'; ?>
---
+++ child-care.php
@@ -0,0 +1,77 @@
+<?php include 'inc/app.php'; ?>
+<link rel="stylesheet" href="css/child-care.css">
+<!-- Custom Banner -->
+<section class="custom_banner">
+    <div class="container">
+        <div class="custom_banner_text">
+            <h2>About Us</h2>
+            <p>
+                <a href="index.php">Home</a>
+                <img src="img/inner.png" alt="">
+                <a href="about.php">About Us</a>
+            </p>
+        </div>
+    </div>
+</section>
+<!-- End Here -->
+<!-- TEXT SECtion -->
+<section class="about_text">
+    <div class="container">
+        <div class="top_heading text-center">
+            <h6 class="blue">Child Care Form</h6>
+            <h4 class="h4">Child Care</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sollicitudin erat vel r <br>
+                honcus sollicitudin. Ma dolor sit amet, consectetur adt, consectetur
+            </p>
+        </div>
+    </div>
+</section>
+<section class="child_care_form">
+    <div class="container">
+        <div class="col-md-6 mx-auto">
+            <div class="form_start">
+                <form action="" method="POST">
+                    <div class="form_row">
+                        <label for="">Email *</label>
+                        <input type="text" name="" id="" placeholder="Your answer">
+                    </div>
+                    <div class="form_row">
+                        <label for="">Email *</label>
+                        <input type="text" name="" id="" placeholder="Your answer">
+                    </div>
+                    <div class="form_row">
+                        <label for="">Email *</label>
+                        <input type="text" name="" id="" placeholder="Your answer">
+                    </div>
+                    <div class="form_row">
+                        <label for="">Email *</label>
+                        <input type="text" name="" id="" placeholder="Your answer">
+                    </div>
+                    <div class="form_row">
+                        <label for="">Email *</label>
+                        <input type="text" name="" id="" placeholder="Your answer">
+                    </div>
+                    <div class="form_row">
+                        <label for="">Email *</label>
+                        <input type="text" name="" id="" placeholder="Your answer">
+                    </div>
+                    <div class="form_row">
+                        <label for="">Email *</label>
+                        <input type="text" name="" id="" placeholder="Your answer">
+                    </div>
+                    <div class="form_row">
+                        <input type="radio" hidden id="test1" name="radio-group" checked>
+                        <label for="test1">Apple</label>
+                    </div>
+                    <div class="form_row">
+                        <input type="radio" hidden id="test2" name="radio-group">
+                        <label for="test2">Peach</label>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- About Text END Here -->
+<!-- contact Section End Here -->
+<?php include 'inc/footer.php'; ?>
